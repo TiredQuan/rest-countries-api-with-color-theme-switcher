0d0d181eb9c0a9cf2e266c3c96c55c2bcf8fb14e
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2022-11-05 13:39:19 +0700

    a

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import './Nav.css'
+
+export default function Nav(){
+    return(
+        <header>
+            <nav>
+                <h1>Where in the world?</h1>
+            </nav>
+        </header>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <MemoryRouter>
     <App />
+    </MemoryRouter>
   </React.StrictMode>
 )
